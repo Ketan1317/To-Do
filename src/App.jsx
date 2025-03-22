ac23ef787013e881bd0941ff47150bdd2d589110
Ketan Goyal <goyalketan1317@gmail.com>
2025-03-23 02:02:28 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <div className="pt-5 select-none text-center">
-      <h1 className="text-4xl font-extrabold text-zinc-200">To-Do List</h1>
+      <h1 className="text-6xl font-extrabold text-zinc-200">To-Do List</h1>
       <form
         onSubmit={(event) => {
           event.preventDefault();
@@ -46,22 +46,23 @@ function App() {
           <input
             type="text"
             onChange={(event) => setNewTask(event.target.value)}
-            className="border-[1px] mt-6 pl-4 ml-7 border-zinc-400"
+            className="border-[1px] hover:border-2 mt-6 pl-4 ml-7 border-zinc-300"
             placeholder="Enter............."
             value={newTask}
           />
+          
           <br />
           <input
             type="submit"
             value="Add Task"
-            className="bg-green-400 mt-4 text-md rounded-2xl font-semibold px-4 py-2"
+            className="bg-green-400 hover:bg-green-700 cursor-pointer mt-4 text-md rounded-2xl font-semibold px-4 py-2"
           />
         </label>
       </form>
-      <p className="text-2xl font-bold text-center mt-4 mb-3">
+      <p className="text-2xl font-bold text-cyan-700 text-center mt-4 mb-3">
         Number of Tasks to-do: {count}
       </p>
-      <div className="p-2 border-2 pb-7 h-fit border-gray-500 ml-10 mr-10 mt-7">
+      <div className="p-6 border-2 pb-7 h-fit border-gray-500 ml-10 mr-10 mt-7">
         <h1 className="font-bold text-cyan-500 text-3xl">Tasks To Do 👇</h1>
 
         {tasks.length > 0 ? (
@@ -69,12 +70,12 @@ function App() {
             {tasks.map((item, index) => (
               <div key={index} className="flex items-center justify-between">
                 <div className="flex items-center justify-center gap-7">
-                  <h2 className="text-3xl font-bold mt-2">{index + 1}.</h2>
+                  <h2 className="text-3xl text-zinc-300 font-bold mt-2">{index + 1}.</h2>
                   <li className="ml-4 text-3xl mt-5 font-bold">{item}</li>
                 </div>
                 <button
                   onClick={() => deleteTask(index)}
-                  className="bg-red-500 mr-4 px-4 py-2 font-semibold text-xs rounded-md"
+                  className="bg-red-500 mr-4 px-4 py-2 cursor-pointer hover:bg-red-800 font-semibold text-xs rounded-md"
                 >
                   Delete
                 </button>
